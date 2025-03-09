5cd66be495a873e95f6a63c7711cb936bcf37003
baongo97 <130117645+baongo97@users.noreply.github.com>
2025-03-09 12:44:21 +0800

    Update ByBitApiKey.java

--- src/main/java/entity/ByBitApiKey.java
+++ src/main/java/entity/ByBitApiKey.java
@@ -1,6 +1,6 @@
 package entity;
 
 public class ByBitApiKey {
-	public static final String API_KEY = "F1axdhhPYZnFnWLk5m";
-	public static final String API_SECRETE = "rk2cMzDHlu6ZMPDeQxHAGiCmvTTHwPTtAQAj";
+	public static final String API_KEY;
+	public static final String API_SECRETE;
 }
